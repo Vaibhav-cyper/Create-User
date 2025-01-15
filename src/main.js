0a786c6355a16725266bee3070a256a6d3ab1a48
Vaibhav Gupta <gvaibhav2435@gmail.com>
2025-01-15 18:08:49 +0530

    create doc added

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { Client, Databases } from 'node-appwrite';
+import { Client, Databases, ID } from 'node-appwrite';
 
 
 const projectID = process.env.PROJECT_ID;
@@ -11,12 +11,16 @@ export default async ({ req, res, log, error }) => {
   const client = new Client()
     .setEndpoint('https://cloud.appwrite.io/v1')
     .setProject(projectID)
-    
+
   const db = new Databases(client);
-  
+
   if (req.method == 'GET') {
-    const response = await db.listDocuments(databaseId,collectionId)
+    const response = await db.listDocuments(databaseId, collectionId)
+    return res.json(response.documents);
+  } else if (req.method == 'POST') {
+    const { payload } = await req.json();
+    const response = await db.createDocument(databaseId, collectionId, ID.unique(), payload, [Permission.read(Role.user('<USER_ID>')), Permission.read(Role.any())])
     return res.json(response.documents);
   }
-
+  
 };
