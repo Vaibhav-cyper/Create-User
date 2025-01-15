b1ec2bbe6b698b1d17bd45ef3f830a7bc1dbc2a5
Vaibhav Gupta <gvaibhav2435@gmail.com>
2025-01-15 18:18:13 +0530

    again create doc added

--- src/main.js
+++ src/main.js
@@ -13,14 +13,14 @@ export default async ({ req, res, log, error }) => {
     .setProject(projectID)
 
   const db = new Databases(client);
-
+  
   if (req.method == 'GET') {
     const response = await db.listDocuments(databaseId, collectionId)
     return res.json(response.documents);
   } else if (req.method == 'POST') {
-    const { payload } = await req.json();
+    const  payload  = await req.json();
     const response = await db.createDocument(databaseId, collectionId, ID.unique(), payload, [Permission.read(Role.user('<USER_ID>')), Permission.read(Role.any())])
-    return res.json(response.documents);
+    return res.json(response.$id);
   }
   
 };
