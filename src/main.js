014f8b58230de64bc838b581b9bb1c9b56a27e8f
Vaibhav Gupta <gvaibhav2435@gmail.com>
2025-01-16 23:57:14 +0530

    createUser event set

--- src/main.js
+++ src/main.js
@@ -13,21 +13,35 @@ export default async ({ req, res, log, error }) => {
     .setProject(projectID)
 
   const db = new Databases(client);
-  
+
   if (req.method == 'GET') {
     const response = await db.listDocuments(databaseId, collectionId)
     return res.json(response.documents);
   } else if (req.method == 'POST') {
-    try{
-      const  payload  =  JSON.parse(req.body);
-      const response = await db.createDocument(databaseId, collectionId, ID.unique(), payload)
+    try {
+      const payload = JSON.parse(req.body);
+      // Extract user details from the payload
+      const userId = payload['$id'];
+      const userName = payload['name'];
+      const userEmail = payload['email'];
+
+      const documentData = {
+        FirstName: userName.split(" ")[0],
+        LastName: userName.split(" ")[1] || "",
+        User_Email: userEmail,
+        Subscription_Status: "Free",
+        User_Created: new Date().toISOString(),
+        Sub: userId,
+      };
+
+      const response = await db.createDocument(databaseId, collectionId, ID.unique(), documentData)
       // Send a response
       return res.json({ message: 'Document created successfully!', response });
     }
-    catch(err){
+    catch (err) {
       log(err.message);
       return res.status(500).json({ error: 'Failed to create document' });
     }
   }
-  
+
 };
